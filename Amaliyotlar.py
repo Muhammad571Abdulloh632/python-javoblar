478a72fff76fd455fdb3989459485d4093c13a8e
Abdulloh <abdullohmuhammad888@gamil.com>
2021-08-14 12:21:08 +0300

    My first  prictise project....

---
+++ Amaliyotlar.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 30 03:41:22 2021
+
+@author: Usman
+"""
+#cars =['toyota', 'mazda', 'hyundai','gm','kia']
+#for car in cars:
+  # print(auto.title())
+#    if car== "gm":
+   #print(car.upper())else:                                               print(car.title())
+
+#print(cars)
+#cars=["mers", "bmw", "volvo","kia"]
+#for auto in cars:
+    #print(auto.title())
+    #if auto == "bmw":
+     #   print(auto.upper())
+    #else:
+       # print(auto.title())
+#login=input("What is your name?\n>>>")
+#if login == "Admin":
+#   print(f"{login} do you want to check user's information?")
+#else:
+    #print(f"Welcome {login.title()}!")
+#x=float(input("enter first number:"))
+#y=float(input("enter second number:"))
+#if x==y:print(f"The numbers equal each others: {x}={y}")
+#11 dars amaliyoti
+#user=int(input("please enter even number:"))
+#son=50
+#if son<0:
+ #   print("rahmat")
+#lse:
+ #   print("yoq bu hato")
+#custumer=int(input("How old are you?"))
+#if custumer <4>60 :
+ #   prise=0
+#elif custumer<18 :
+ #   prise=1000 
+#if custumer>18 :
+#else:
+ #   prise=20000
+#    print(f"Bepul, {prise}")
+#son=-87
+ # if son<0:
+ #   print("manfiy son")
+#else:
+   #  print("musbat son")
+12 dars amaliyoti
+
